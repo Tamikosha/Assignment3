1a7d465ab8c3ab5c6d15f2f639d922a6e365fa98
Tamikosha <56810672+Tamikosha@users.noreply.github.com>
2022-03-08 23:41:00 +0600

    /

--- webws/src/main/java/CalculatorLoc.java
+++ webws/src/main/java/CalculatorLoc.java
@@ -27,18 +27,13 @@ public class CalculatorLoc {
 //        cs.getCalculatorPort(ao);
         double a = Double.parseDouble("request");
 
-//        // создаем ссылку на wsdl описание
+        
         URL url = new URL("http://alex-strix-notebook:8080/JavaEE-WS-1.0-SNAPSHOT/CalculatorService");
-//
-//        // Параметры следующего конструктора смотрим в самом первом теге WSDL описания - definitions
-//        // 1-ый аргумент смотрим в атрибуте targetNamespace
-//        // 2-ой аргумент смотрим в атрибуте name
+     
         QName qname = new QName("http://iitu.kz/", "CalculatorService");
-//
-//        // Теперь мы можем дотянуться до тега service в wsdl описании,
+
         Service service = Service.create(url, qname);
-//        // а далее и до вложенного в него тега port, чтобы
-//        // получить ссылку на удаленный от нас объект веб-сервиса
+
         CalculatorService calculatorService = new CalculatorService(url, qname);
         Calculator calculator = calculatorService.getCalculatorPort();
         System.out.println(calculator.additionOperator(1, 2));
